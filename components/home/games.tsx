fa631c55674aea1bc926922a6cac99660c62c2a4
Kevin Lai <kevinlaisoftware@gmail.com>
2023-12-15 17:44:53 -0500

    feat: intersection observer

--- components/home/games.tsx
+++ components/home/games.tsx
@@ -1,12 +1,38 @@
+'use client'
 import Image from "next/image";
 import crash from "../../images/crash.jpg";
 
+import { useEffect, useRef } from "react";
+
 const Games = () => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate-fade");
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    if (divRef.current) {
+      observer.observe(divRef.current);
+    }
+
+    return () => {
+      if (divRef.current) {
+        observer.unobserve(divRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="bg-neutral p-5">
-      <div className="rounded-3xl bg-primary">
+      <div ref={divRef} className="rounded-3xl bg-primary animate-once animate-ease-in">
         <div className="p-5">
-          <Image src={crash} alt="Crash" className="rounded-2xl"/>
+          <Image src={crash} alt="Crash" className="rounded-2xl" />
           <h1 className="text-2xl text-neutral pb-5 pt-5">
             Crowdsourced Stewarding
           </h1>
