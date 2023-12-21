e86c8d174b5e130aa8dad4fe95fe2e970159adea
Kevin Lai <kevinlaisoftware@gmail.com>
2023-12-21 12:17:09 -0500

    add clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import localFont from 'next/font/local'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const silkScreen = localFont({ src: '../style/fonts/Silkscreen-Regular.ttf' } )
-
+const silkScreen = localFont({ src: "../style/fonts/Silkscreen-Regular.ttf" });
 
 export const metadata: Metadata = {
-  title: 'RaceTree',
-  description: 'A LinkTree for Sim Racing Enthusiasts',
-}
+  title: "RaceTree",
+  description: "A LinkTree for Sim Racing Enthusiasts",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={silkScreen.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={silkScreen.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
