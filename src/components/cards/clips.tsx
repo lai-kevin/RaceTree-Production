790fa9aa9d6603c98c7b3caa294623627ae9b458
Kevin Lai <kevinlaisoftware@gmail.com>
2024-01-05 16:08:00 -0500

    add clips layout

--- src/components/cards/clips.tsx
+++ src/components/cards/clips.tsx
@@ -1,13 +1,28 @@
 const Clips = () => {
-    return (
-    <div className="bg-base-100 rounded-2xl m-5 h-[600px]">
-        <div className="card-body p-5">
-          <h1 className="card-title text-xl">Clips</h1>
-          
+  return (
+    <div className="bg-base-100 rounded-2xl m-5">
+      <div className="card-body p-5">
+        <h1 className="card-title text-xl">Clips</h1>
+        <div className="w-full border-opacity-50 h-[500px]">
+          <div className="grid h-20 card bg-base-300 rounded-box place-items-center">
+            content
+          </div>
+          <div className="divider" />
+          <div className="grid h-20 card bg-base-300 rounded-box place-items-center">
+            content
+          </div>
+          <div className="divider" />
+          <div className="grid h-20 card bg-base-300 rounded-box place-items-center">
+            content
+          </div>
+          <div className="divider" />
+          <div className="flex justify-center">
+            <button className="btn">View More</button>
+          </div>
         </div>
       </div>
-    );
-  };
-  
-  export default Clips;
-  
+    </div>
+  );
+};
+
+export default Clips;
