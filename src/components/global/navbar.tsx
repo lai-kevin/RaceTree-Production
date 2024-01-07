253ab360aae06ca3ae3524b5c890b5eec36265ba
Kevin Lai <kevinlaisoftware@gmail.com>
2024-01-07 18:16:20 -0500

    remove profile button

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,44 +1,49 @@
+const ProfileButton = () => {
+  return (
+    <div className="dropdown dropdown-end">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost btn-circle avatar"
+      >
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+          />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li>
+          <a>Settings</a>
+        </li>
+        <li>
+          <a>Logout</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-100" style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 999 }}>
+    <div
+      className="navbar bg-base-100"
+      style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 999 }}
+    >
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">RaceTree</a>
       </div>
-      <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-
-            </li>
-          </ul>
-        </div>
-      </div>
+      <div className="flex-none"></div>
     </div>
   );
 };
